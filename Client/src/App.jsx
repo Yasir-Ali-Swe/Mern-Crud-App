11ef9f8dc2ad22b191bf2059979e71af60361197
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-09-20 18:18:57 +0500

    Perform Routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
-  return <h1>Crud App</h1>;
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: "Home Page",
+    },
+    {
+      path: "/add",
+      element: "Add Page",
+    },
+    {
+      path: "/update",
+      element: "Update Page",
+    },
+  ]);
+  return <RouterProvider router={route}></RouterProvider>;
 }
 
 export default App;
