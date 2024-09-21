2bb6df06989524a9f48eaef8ef083c8e3186da97
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-09-21 10:26:58 +0500

    Calling the Apis and populate the forms with Data

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       element: <AddUser />,
     },
     {
-      path: "/update",
+      path: "/update/:id",
       element: <UpdateUser />,
     },
   ]);
--- Client/src/Components/Users/User.jsx
+++ Client/src/Components/Users/User.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./User.css";
 const User = () => {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/getAll");
+        setUser(response.data);
+      } catch (error) {
+        console.log("Error occurred => ", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="UserTable">
       <Link to={"/add"} className="AddBtn">
@@ -17,17 +30,22 @@ const User = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Yasir Ali</td>
-            <td>yasir@gmail.com</td>
-            <td>
-              <button>Delete</button>
-              <Link to={"./update"} className="UpdateBtn">
-                Update
-              </Link>
-            </td>
-          </tr>
+          {users.map((user, index) => {
+            return (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{`${user.fname} ${user.lname}`}</td>
+                <td>{user.email}</td>
+                <td>
+                  {" "}
+                  <button>Delete</button>
+                  <Link to={`./update/${user._id}`} className="UpdateBtn">
+                    Update
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
